2de7f2834481f71ccb395d869f0e8bd1b32e2c62
HTT <nguyenhuy.86nt@gmail.com>
2020-05-14 09:04:57 +0700

    Cap nhat trang Home va tao Model NguoiDung.cs

---
+++ QuanLyNhanSu/QuanLyNhanSu/Models/NguoiDung.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyNhanSu.Models
+{
+    public class NguoiDung
+    {
+        public string MaND { get; set;}
+        public string MatKhau{ get; set;}
+    }
+}
